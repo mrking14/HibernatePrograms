111a8e9d02d16b21b5e66591a296e0f21869ecb1
MasoomRaza <0000masoomraza@gmail.com>
2023-06-21 08:25:35 +0530

    Fetch method use in one to many Program.

--- hibernateMapping/src/main/java/one2many/Question.java
+++ hibernateMapping/src/main/java/one2many/Question.java
@@ -4,55 +4,59 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Question {
 	@Id
-	@Column(name ="question_id")
+	@Column(name = "question_id")
 	private int qId;
 	private String question;
-	
+
+//	 we used this for defining Loading Method.  like if we want to load joined data before calling "fetch = FetchType.EAGER" else that will be by default Lazy loading method .
+
 //	@OneToMany     --- If You want to add join column in Question table 
-	@OneToMany(mappedBy = "question") // Here if you do not want to create extra column in table.
-	
+	@OneToMany(mappedBy = "question", fetch = FetchType.EAGER) // Here if you do not want to create extra column in
+																// table.
+
 	private List<Answer> answer;
-	
+
 	public Question() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Question(int qId, String question, List<Answer> answer) {
 		super();
 		this.qId = qId;
 		this.question = question;
 		this.answer = answer;
 	}
-	
+
 	public int getqId() {
 		return qId;
 	}
+
 	public void setqId(int qId) {
 		this.qId = qId;
 	}
+
 	public String getQuestion() {
 		return question;
 	}
+
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	
+
 	public List<Answer> getAnswer() {
 		return answer;
 	}
-	
+
 	public void setAnswer(List<Answer> answer) {
 		this.answer = answer;
 	}
-	
-	
-	
-	
 
 }
